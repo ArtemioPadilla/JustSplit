6f7382ea03a258394fec41e03270c4bb4ffb1c4b
Artemio Padilla <artemiopadilla@gmail.com>
2025-05-20 23:42:08 -0600

    feat: update Firebase deployment workflow and configuration for Next.js integration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export', // REMOVE this line for Firebase Next.js integration
   images: {
     unoptimized: true,
   },
